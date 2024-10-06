0e17a685b34e72a6a9b449e56a9ac7d7118f32bf
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2024-10-06 22:04:14 +0300

    formated

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from datetime import timedelta
 
 from expense_tracker import (
-    PaymentTracker, UpdatableCurrencyConverter, ExpenseSummary,
+    PaymentTracker,
+    UpdatableCurrencyConverter,
+    ExpenseSummary,
     Currency,
 )
 from my_payments import MY_PAYMENTS
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     tracker = PaymentTracker()
     currency_converter = UpdatableCurrencyConverter()
     summary = ExpenseSummary(tracker, currency_converter)
@@ -16,7 +18,11 @@ if __name__ == '__main__':
 
     period = timedelta(days=365)
     currency = Currency.USD
-    for charge in sorted(summary.charges(period, currency), key=lambda x: x.money_value.amount, reverse=True):
+    for charge in sorted(
+        summary.charges(period, currency),
+        key=lambda x: x.money_value.amount,
+        reverse=True,
+    ):
         print(charge)
 
     print(summary.total(period, currency))
